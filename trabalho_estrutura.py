9dd28752f7a7c66b56f182e0c0d032d53c7e7f2d
Luna <rlunamgt@gmail.com>
2025-05-30 20:29:11 -0300

    função de maior ou menor IP

--- trabalho_estrutura.py
+++ trabalho_estrutura.py
@@ -3,3 +3,12 @@ class Computador:
         self.ip = ip
         self.esquerdo = None
         self.direito = None
+
+def inserir(raiz, ip):
+    if raiz is None:
+        return Computador(ip)
+    if ip < raiz.ip:
+        raiz.esquerdo = inserir(raiz.esquerdo, ip)
+    else:
+        raiz.direito = inserir(raiz.direito, ip)
+    return raiz
